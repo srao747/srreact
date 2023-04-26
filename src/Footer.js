872e6ae446dbce305605da7d2d95b017fd9fd261
srao747 <109792471+srao747@users.noreply.github.com>
2023-04-26 09:47:50 +0530

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    
+        <>
+       
+            <footer className="text-center text-lg-start bg-light text-muted pt-5">
+            
+            <div className="container text-center text-md-start mt- ">
+                
+  
+            </div>
+            <div className="text-center p-4 bg-dark text-white">
+                Assignment
+                
+            </div>
+           
+            </footer>
+       
+        </>
+    
+  )
+}
+
